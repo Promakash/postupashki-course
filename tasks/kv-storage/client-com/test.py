a848162c3de9b8fa3f19290e792aa27e10751a8c
Promakash <roma_kash@inbox.ru>
2024-05-13 02:25:07 +0300

    [client-com] Python tests finished.Fixed testcase for get method of single element. Added testcase for get method for all entries at once. [Makefile] Reworked to multithreading to evade trouble when start of server blocks thread and tests are not starting

--- tasks/kv-storage/client-com/test.py
+++ tasks/kv-storage/client-com/test.py
@@ -12,13 +12,16 @@ def test_http_kv():
 
     kv = dict()
 
+    #Tests for json method
     for i in range(1, 11):
         key = f'{i}_key'
         value = f'{i}_value'
 
         resp = requests.post(f'{kv_addr}/entry', json={"key": key, "value": value})
         assert resp.status_code == 200
+        kv[key] = value
 
+    #Tests for get method of single element
     for i in range(1, 11):
         key = f'{i}_key'
         value = f'{i}_value'
@@ -26,9 +29,16 @@ def test_http_kv():
         resp = requests.get(f'{kv_addr}/entry', params={"key": key})
         assert resp.status_code == 200
 
-        assert value == json.loads(resp.content.decode())["value"]
+        assert value == json.loads(resp.content.decode())[-1]
 
+    #Test for case when entry is requested that is not in storage
     resp = requests.get(f'{kv_addr}/entry', params={"key": "lolkek228"})
     assert resp.status_code == 404
 
-test_http_kv();
+    #Test for get method of all entries at once
+    resp = requests.get(f'{kv_addr}/entries')
+    entries = json.loads(resp.content.decode())
+    for entry in entries:
+        del kv[entry['key']]
+    assert resp.status_code == 200
+    assert len(kv) == 0
